37975d335c7972995755d1d4694932d0d22119a4
admin <admin@DESKTOP-Q744UO1>
2020-08-03 11:12:30 +0900

    Initral commit

---
+++ First/src/hello/hellojava.java
@@ -0,0 +1,21 @@
+/*-위에는 주석을 이용해서 프로그램 설명을 해놓으면 유용하다
+ * date :2020.06.26
+ * Author :홍길동
+ * 설명 : 첫번째 자바프로그램입니다
+ * Version : 1.0
+ */
+
+package hello;
+
+public class hellojava {
+
+	public static void main(String[] args) {
+		// 주석입니다. 코드를 설명한다. 사람이 보기위함(컴퓨터인식x)
+		System.out.println("Hello,Java");
+		/*
+		 * 이 사이공간은 전부 주석처리된다 여러줄 주석
+		 * 
+		 */
+		System.out.println("영애");
+	}
+}
