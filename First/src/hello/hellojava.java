69a39d3eec67c9a9f890b19475cc1fb9966ec472
admin <admin@DESKTOP-Q744UO1>
2020-08-03 11:32:59 +0900

    hello.java를 또 수정

--- First/src/hello/hellojava.java
+++ First/src/hello/hellojava.java
@@ -13,6 +13,7 @@ public class hellojava {
 		// 주석입니다. 코드를 설명한다. 사람이 보기위함(컴퓨터인식x)
 		System.out.println("헬로우 자바!");
 		System.out.println("수정했음!");
+		System.out.println("깃허브 올리고 수정함!");
 		/*
 		 * 이 사이공간은 전부 주석처리된다 여러줄 주석
 		 * 
